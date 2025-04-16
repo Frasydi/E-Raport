ae71440899f80b92958232580025c9a91d776ca2
fykri <dzulfikriyunus@gmail.com>
2025-04-16 18:46:02 +0800

    menambahkan pages login

--- frontEnd/src/pages/Login.jsx
+++ frontEnd/src/pages/Login.jsx
@@ -1,51 +1,35 @@
 const Login = () => {
     return (
-      <div class="bg-gray-100 flex justify-center items-center h-screen">
-        <div className="w-1/2 h-screen hidden lg:block">
-          <img src="https://placehold.co/800x/667fff/ffffff.png?text=Your+Image&font=Montserrat" alt="Tes" className="object-cover w-full h-full"/>
+        <div class="w-full h-screen flex">
+            <img
+                src="./src/assets/image/LoginImg.png"
+                alt="background"
+                class="object-cover object-center h-screen w-7/12"
+            />
+            <div class="bg-white flex flex-col justify-center items-center w-5/12 shadow-lg">
+                <h1 class="text-3xl font-bold text-orange-500 mb-2">LOGIN</h1>
+                <div class="w-1/2 text-center">
+                    <input
+                        type="text"
+                        name="username"
+                        placeholder="username"
+                        autocomplete="off"
+                        class="shadow-md border w-full h-10 px-3 py-2 text-orange-500 focus:outline-none focus:border-orange-500 mb-3 rounded"
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="password"
+                        autocomplete="off"
+                        class="shadow-md border w-full h-10 px-3 py-2 text-orange-500 focus:outline-none focus:border-orange-500 mb-3 rounded"
+                    />
+                    <button class="bg-orange-500 hover:bg-orange-600 text-white px-3 py-1 rounded text-lg focus:outline-none shadow">
+                        Sign In
+                    </button>
+                </div>
+            </div>
         </div>
-        <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
-          <h1 className="text-2xl font-semibold mb-4">Login</h1>
-          <form action="#" method="POST">
-            <div class="mb-4">
-              <label for="username" className="block text-gray-600">Username</label>
-              <input
-                  type="text"
-                  id="username"
-                  name="username"
-                  className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
-                  autoComplete="off"
-              />
-            </div>
-            <div className="mb-4">
-              <label for="password" className="block text-gray-600">Password</label>
-              <input 
-                  type="password" 
-                  id="password" 
-                  name="password" 
-                  className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" 
-                  autocomplete="off"/>
-            </div>
-            <div className="mb-4 flex items-center">
-              <input 
-                  type="checkbox" 
-                  id="remember" 
-                  name="remember" 
-                  className="text-blue-500"/>
-              <label for="remember" className="text-gray-600 ml-2">Remember Me</label>
-            </div>
-            <div className="mb-6 text-blue-500">
-              <a href="#" className="hover:underline">Forgot Password?</a>
-            </div>
-            <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full">Login</button>
-          </form>
-          <div className="mt-6 text-blue-500 text-center">
-            <a href="#" className="hover:underline">Sign up Here</a>
-          </div>
-        </div>
-      </div>
-    )
-}
+    );
+};
 
-export default Login
-
+export default Login;
